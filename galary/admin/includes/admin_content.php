a11cf5ffaa5d3162b24401429d09d66c0fdbaa5b
Declan <decofeco29@gmail.com>
2018-06-09 15:15:41 +0100

    Update database.php to use new mysqli object

--- galary/admin/includes/admin_content.php
+++ galary/admin/includes/admin_content.php
@@ -7,6 +7,13 @@
                 Admin
                 <small>Subheading</small>
             </h1>
+            
+            <?php
+            $sql = "SELECT * FROM users WHERE id = 1";
+            $result = $database->query($sql);
+            $users_found = mysqli_fetch_array($result);
+            echo print_r($users_found);
+            ?>
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
--- galary/admin/includes/init.php
+++ galary/admin/includes/init.php
@@ -1,4 +1,5 @@
 <?php
 include("new_config.php"); // usually called config.php as standard practice but this was causing conflicting errors with php mysqli so renamed it as shown
+include("database.php");
 ?>
 
